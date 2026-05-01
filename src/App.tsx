7a5955dd1bb584dd3b0e38151ff61ef80000c958
IAMTOYO-AI <toyosiodewenwa@gmail.com>
2026-05-01 15:46:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const MEMORIES: Memory[] = [
   { 
     id: 1, 
      url: 'https://res.cloudinary.com/dsuutxrh8/image/upload/v1777646536/memory3_zq0wsq.jpg',
-    caption: 'The day we first laughed together.', 
+    caption: 'The day we last laughed together.', 
     story: 'That Tuesday afternoon wasn\'t supposed to be special, but then you told that ridiculous joke about the penguin. I don\'t even remember the punchline anymore, but I remember how your eyes crinkled at the corners. It was the first time I realized that your laughter was my favorite sound in the world.\n\nWe sat on that bench for hours, ignoring the world around us, just caught in the orbit of our own joy. Every time I see this picture, I can still feel the warmth of that sun and the lightness in my chest that hasn\'t gone away since.',
     date: 'Spring 2023' 
   },
